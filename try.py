f7fec12b111a8a7db142e9b834b3af870a24e197
samoula007 <samy.arezki@mail.mcgill.ca>
2023-09-03 20:02:58 -0400

    Queue properly constructed, now at number3

--- try.py
+++ try.py
@@ -1,6 +1,45 @@
-import heapq
+# Import the heap functions from python library
+from heapq import heappush, heappop, heapify
 import math
 import csv
+
+# A class for Max Heap
+# Need to negate the values in order to get a Max
+# Slightly modified implementation from: https://www.geeksforgeeks.org/binary-heap/
+class MaxHeap:
+	# Constructor to initialize a heap
+	def __init__(self):
+		self.heap = []
+
+	def parent(self, i):
+		return (i-1)/2
+	
+	# Inserts a new key 'k'
+	def insertKey(self, k,j):
+		heappush(self.heap, (-k,j))		
+
+	# Decrease value of key at index 'i' to new_val
+	# It is assumed that new_val is smaller than heap[i]
+	def decreaseKey(self, i, new_val):
+		self.heap[i] = new_val
+		while(i != 0 and self.heap[self.parent(i)] > self.heap[i]):
+			# Swap heap[i] with heap[parent(i)]
+			self.heap[i] , self.heap[self.parent(i)] = (
+			self.heap[self.parent(i)], self.heap[i])
+			
+	# Method to remove max element from max heap
+	def extractRoot(self):
+		return heappop(self.heap)
+
+	# This function deletes key at index i. It first reduces
+	# value to minus infinite and then calls extractMax()
+	def deleteKey(self, i):
+		self.decreaseKey(i, float("-inf"))
+		self.extractRoot()
+
+	# Get the minimum element from the heap
+	def getRoot(self):
+		return self.heap[0]
 
 def f(delta_i, alpha_i, gamma_i, p_i):
     """
@@ -15,7 +54,6 @@ def f(delta_i, alpha_i, gamma_i, p_i):
     #compute the expected profit
     return beta_i * (p_i - delta_i) + (1 - beta_i)*(1 - alpha_i) * p_i
 
-# NOTE: Contrary to the authors' pseudocode, I will additionally input delta and supply to the data
 def greedOffer( alpha, gamma, p, delta, supply):
     """
     :param alpha: list of floats, churn out prob for each subscriber
@@ -35,82 +73,65 @@ def greedOffer( alpha, gamma, p, delta, supply):
     A = []
 
     #3
-    #Construct the priority queues 1,...,k for each offer
+    #Construct the list Q of maxHeaps 1,...,k for each offer
     k = len(delta)
     Q = [ [] for i in range(k)] #The queue of queues Q_j
     for i in range(k):
+        heap = MaxHeap()
         for j in range(n):
             priority = f(delta[i], alpha[j], gamma[j], p[j])
-            heapq.heappush(Q[i], (priority, j))
-    print (Q)
+            heap.insertKey(priority,j)
+		# max = - Q[i].getRoot()[0] --> peek the root positive value
+        # customer = Q[i].getRoot()[1] --> peek the customer number
+        # max = -heap.extractRoot()[0] --> pop the root positive value
+        Q[i] = heap
+
     #4
     #Construct the lookup table T of size n x k
     #Each element of T references the position of a subscriber i in each queue j
     T = [ [0 for i in range(k)] for j in range(n)]
-    for i in range(n):
-        for j in range(k):
-            priority = f(delta[j], alpha[i], gamma[i], p[i])
-            T[i][j] = Q[j].index((priority, i))
+    #Loop through the queues
+    #whenever we see customer i for queue j, we update T[i][j] to the position of i in Q[j]
+    for j in range(k):
+        for i in range(n):
+            customer = Q[j].getRoot()[1]
+            T[j][customer] = i
+    print(T)
     
+    
+"""
     #5
     #Construct the array L of size k
     L = [0 for i in range(k)]
     #Each element j of L is the head of each queue j in Q
-    for i in range(k):
-        L[i] = Q[i][0][1]
-        print (L[i])
+    
     
     #6
     #While the total number of offers is > 0, and the set of subscribers is not empty
     while sum(supply) > 0 and len(S) > 0:
-        #initialize prios
-        top_prio = float('-inf')
-        curr_j = -1
-        curr_i = -1
-        # find the pair (i,j) with the highest priority
-        # iterate through Q, and the queue with head of highest value is the one with highest prio 
-        # the pair (i,j) is then given by the current j (representing offer) and the head of the queue (representing subscriber)
-        for j in range(k):
-            if len(Q[j]) == 0:
-                continue
-            largest = heapq.nlargest(1,Q[j])[0]
-            if L[j] != -1 and largest[0] > top_prio:
-                top_prio = largest[0]
-                curr_j = j
-                curr_i = largest[1]
-
+        
         # Append it to A
-        A.append((curr_i, curr_j))
+        
         # Remove the customer i from S
-        S.remove(curr_i)
+        
         # Remove the customer i from all queues
         #the custommer number is the second element of the tuple in the queue
-        for j in range(k):
-            priority = f(delta[j], alpha[curr_i], gamma[curr_i], p[curr_i]) 
-            if (priority, curr_i) in Q[j]:
-                Q[j].remove((priority, curr_i))
-            
+        
         # Update the array L
-        L[curr_j] = Q[curr_j][0][1]
+        
 
         # Update the lookup table T
-        for j in range(k):
-            for i in range(n):
-                priority = f(delta[j], alpha[i], gamma[i], p[i])
-                if (priority, i) in Q[j]:
-                    T[i][j] = Q[j].index((priority, i))
+        
 
         # Update K and offer number
-        supply[curr_j] -= 1
+        
         # If there are no more offers of type j, remove Q_j from the set of queues.
         #this is also probably shit
-        if supply[curr_j] == 0:
-            Q[curr_j] = []
+        
     #7
     #Return the solution set A
     return A
-     
-
+"""
 ############################################################################################################
 #Testing naive implementation
 
@@ -143,6 +164,3 @@ supply = [int(i) for i in supply_s]
 A = greedOffer(alpha, gamma, p, delta, supply)
 print("############################################################################################################")
 print (A)
-#need to compute the profit
-#need to check if the solution is feasible
-#build test suite for that?
